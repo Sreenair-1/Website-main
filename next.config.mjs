90600573bb7a065137d284b51bbc5625bf9f95bf
Sreenair-1 <sreeharinair514@gmail.com>
2026-04-24 02:36:14 +0530

    Update .gitignore to include .env and enhance next.config.mjs with experimental settings and turbopack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const nextConfig = {
+  experimental: {
+    cpus: 1,
+    workerThreads: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: __dirname,
   },
 }
 
